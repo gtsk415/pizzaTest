390c98d1b9f2ae78a666af539b0f53e7ae4b332a
gtsk415 <print.bistro@mail.ru>
2024-09-20 16:36:42 +0300

    w200924

--- js/cart.js
+++ js/cart.js
@@ -7,7 +7,7 @@ window.addEventListener('click', function(e) {
     const productInfo = {
       id: card.dataset.id,
       imgSrc: card.querySelector('.product-img').getAttribute('src'),
-      title: card.querySelector('.cart-item__title').innerText,
+      title: card.querySelector('.card__title').innerText,
       itemsInBox: card.querySelector('.card__descr').innerText,
       weight: card.querySelector('.price__weight').innerText,
       currency: card.querySelector('.price__currency').innerText,
@@ -16,21 +16,36 @@ window.addEventListener('click', function(e) {
     };
 
 
-    const cartItemHTML = `<div class="cart">
-        <div class="cart-item" data-id=${productInfo.id}>
-            <div class="cart-item__top">
-              <div class="cart-item__img">
-                <img src=${productInfo.imgSrc} alt="">
-              </div>
-              <div class="cart-item__desc">
-                <div class="cart-item__title">${productInfo.title}</div>
-                <div class="cart-item__weight">${productInfo.weight}</div>
-              </div>
-            </div>
-          </div>
-        </div>`;
-  }
+    const cartHtml = `<div class="cart-item" data-id=${productInfo.id}>
+    <div class="cart-item__img">
+      <img src=${productInfo.imgSrc} alt="">
+    </div>
+    <div class="cart-item__content">
+      <div class="cart-item__desc">
+        <div class="cart-item__title">${productInfo.title}</div>
+        <div class="cart-item__weight">${productInfo.weight}</div>
 
-    cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+        <!-- cart-item__details -->
+        <div class="cart-item__details">
+
+          <div class="items items--small counter-wrapper">
+            <div class="counter__control" data-action="minus">-</div>
+            <div class="counter__current" data-counter="">${productInfo.counter}</div>
+            <div class="counter__control" data-action="plus">+</div>
+          </div>
+
+          <div class="price">
+            <div class="price__currency">${productInfo.currency}</div>
+          </div>
+
+        </div>
+        <!-- // cart-item__details -->
+
+      </div>
+    </div>
+  </div>`;
+
+    cartWrapper.insertAdjacentHTML('beforeend', cartHtml);
+  };
 
 })
